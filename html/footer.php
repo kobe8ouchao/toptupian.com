9c5f8dfa49f019a61fe44fd90ff3707fa4972bd3
ouchao <kobe824ouchao@gmail.com>
2025-12-07 19:41:44 +0800

    Delete html directory

--- html/footer.php
+++
@@ -1,4 +0,0 @@
-<!-- footer-->
-<a id="elevator" href="#" onclick="return false;" class="Indicator off btn wbtn"><strong> 回到顶部</strong><span style="border:1px solid #bbb"></span></a>
-	</body>
-</html>
